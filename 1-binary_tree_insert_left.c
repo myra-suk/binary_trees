2d4abb2563e302c6bdfe22eb52b094f27f98f597
myra-suk <myrasanaa17@gmail.com>
2023-03-02 11:14:29 +0000

    Modified the code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - create a node
+ * binary_tree_insert_left - Inserts a node as a left child of another node
  *
  *@parent: pointer to parent
  *@value: value
