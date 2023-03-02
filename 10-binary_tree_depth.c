67f69e0c072806be42d52e95f4f9dbb2f52001ec
myra-suk <112114209+myra-suk@users.noreply.github.com>
2023-03-02 20:51:04 +0300

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,15 +9,15 @@
 
 size_t binary_tree_depth2(const binary_tree_t *tree)
 {
-  int cont1 = 0;
+int cont1 = 0;
 
-  if (tree == NULL)
-    return (0);
+if (tree == NULL)
+return (0);
 
-  if (tree->parent != NULL)
-    cont1 += binary_tree_depth2(tree->parent);
+if (tree->parent != NULL)
+cont1 += binary_tree_depth2(tree->parent);
 
-  return (cont1 + 1);
+return (cont1 + 1);
 }
 
 /**
@@ -30,8 +30,8 @@ size_t binary_tree_depth2(const binary_tree_t *tree)
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
+if (tree == NULL)
+return (0);
 
-  return (binary_tree_depth2(tree) - 1);
+return (binary_tree_depth2(tree) - 1);
 }
